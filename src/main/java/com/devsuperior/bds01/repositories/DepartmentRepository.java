a1066c3c1f8e89a88dfd583eacda6b4ca0cdc25a
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-02-06 12:33:45 -0300

    Tests passando, OK

--- src/main/java/com/devsuperior/bds01/repositories/DepartmentRepository.java
+++ src/main/java/com/devsuperior/bds01/repositories/DepartmentRepository.java
@@ -2,8 +2,8 @@ package com.devsuperior.bds01.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.bds01.entities.Employee;
+import com.devsuperior.bds01.entities.Department;
 
-public interface DepartmentRepository  extends JpaRepository<Employee, Long> {
+public interface DepartmentRepository  extends JpaRepository<Department, Long> {
 
 }
--- src/main/java/com/devsuperior/bds01/repositories/EmployeetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.bds01.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.bds01.entities.Department;
-
-public interface EmployeetRepository  extends JpaRepository<Department, Long> {
-
-}
